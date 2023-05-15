426564a06407dceed02120a26643ece583093a87
bprathamesh20 <bprathamesh2003@gmail.com>
2023-05-16 01:35:10 +0530

    resolved profilePopUp routing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../assets/logo.png';
 import { useNavigate } from 'react-router-dom';
-import ProfilePopUp from './common/ProfilePopUp';
+import ProfilePopUp from './common/profilePopUp'
 
 export default function Navbar(props) {
   const isLoggedIn = props.isLoggedIn;
