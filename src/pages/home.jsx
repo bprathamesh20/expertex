bcd88663573efe774c743c68bb94b09b3da7a565
bprathamesh20 <bprathamesh2003@gmail.com>
2023-05-16 22:28:12 +0530

    page reload issue attended (not resolved)

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,25 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import HomeComponent from '../components/HomeComponent'
 import { onAuthStateChanged } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import { auth } from '../firebaseConfig'
 
 export default function Home() {
-    let navigate = useNavigate()
-    useEffect(()=>{
-        onAuthStateChanged(auth,(res) => {
-            if(!res?.accessToken){
-                navigate('/')
+  const navigate = useNavigate()
 
-            }
-        })
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigate('/')
+      }
     })
+
+    return () => unsubscribe()
+  }, [])
+
   return (
-    
-    <div><HomeComponent/></div>
+    <div>
+      <HomeComponent />
+    </div>
   )
 }
